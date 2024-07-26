e7adf7e023185da4398804e4aaa100c7ffc771e0
poteto <naonyan1135@gmail.com>
2024-07-26 22:01:52 +0900

    とりあえず

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const path = require("path")
 const mongoose = require("mongoose")
 const Campground = require("./models/campground") //* 大文字ね
+const campground = require("./models/campground")
 
 mongoose
   .connect("mongodb://localhost:27017/yelpCamp", {
@@ -22,11 +23,22 @@ app.get("/", (req, res) => {
   res.render("home")
 })
 
-app.get("/makecampground", async (req, res) => {
-  const camp = new Campground({ title: "私の庭", description: "気軽に安くキャンプ" })
-  await camp.save()
-  res.send(camp)
+app.get("/campgrounds",async(req,res)=>{
+  const campgrounds = await Campground.find({})
+res.render("campgrounds/index",{campgrounds})
 })
+
+app.get("/campgrounds/:id",async(req,res)=>{
+  const {id} = req.params
+const campground = await Campground.findById(id)
+  res.render("campgrounds/show",{campground})
+})
+
+// app.get("/makecampground", async (req, res) => {
+//   const camp = new Campground({ title: "私の庭", description: "気軽に安くキャンプ" })
+//   await camp.save()
+//   res.send(camp)
+// })
 
 app.listen(3000, () => {
   console.log("port3000で起動中")
