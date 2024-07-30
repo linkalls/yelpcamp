d4b4a64108ad4190766bee9e332387e85a460fc6
poteto <naonyan1135@gmail.com>
2024-07-31 02:38:35 +0900

    地図

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -44,7 +44,7 @@ module.exports.createCampground = async (req, res) => {
   campground.images = req.files.map((f) => ({ url: f.path, filename: f.filename }))
   campground.author = req.user._id
   await campground.save()
-  console.log(campground)
+  // console.log(campground)
   req.flash("success", "新しいキャンプ場を登録しました")
   res.redirect(`/campgrounds/${campground._id}`)
 }
