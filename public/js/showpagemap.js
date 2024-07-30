ab31fb91cc40104768e8376e1b5d2eb5f29c761d
poteto <naonyan1135@gmail.com>
2024-07-31 02:52:17 +0900

    エラーハンドリングはない

--- public/js/showpagemap.js
+++ public/js/showpagemap.js
@@ -3,6 +3,11 @@
   const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 9, // starting zoom
+    center: campground.geometry.coordinates,// starting position [lng, lat]
+    zoom: 8, // starting zoom
   });
+
+     // Create a default Marker and add it to the map.
+     const marker1 = new mapboxgl.Marker()
+     .setLngLat(campground.geometry.coordinates)
+     .addTo(map);
