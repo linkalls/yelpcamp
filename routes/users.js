1583067f6dcf277edb2367824c5cc4003f8218cc
poteto <naonyan1135@gmail.com>
2024-07-30 14:40:44 +0900

    ほえ

--- routes/users.js
+++ routes/users.js
@@ -7,15 +7,19 @@ router.get("/register", (req, res) => {
   res.render("users/register")
 })
 
-router.post("/register", async (req, res) => {
+router.post("/register", async (req, res, next) => {
   try {
     const { username, email, password } = req.body
     const user = new User({ username, email }) //* passwordは渡さない
     // console.log(user)
-    const registerUser = await User.register(user, password)
-    // console.log(registerUser)
-    req.flash("success", "YelpCampへようこそ")
-    res.redirect("/campgrounds")
+    const registeredUser = await User.register(user, password)
+    req.login(registeredUser, (err) => {
+      if (err) {
+        return next(err)
+      }
+      req.flash("success", "YelpCampへようこそ")
+      res.redirect("/campgrounds")
+    })
   } catch (e) {
     req.flash("error", e.message)
     res.redirect("/register")
