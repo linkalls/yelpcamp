05dbe52d996dcaadabc731f4d5dd84cdd60894b1
poteto <naonyan1135@gmail.com>
2024-07-31 00:09:34 +0900

    cloudinary

--- routes/campground.js
+++ routes/campground.js
@@ -3,18 +3,19 @@ const router = express.Router()
 const catchAsync = require("../utils/catchAsync")
 const { isLoggedIn, isAuthor, validateCampground } = require("../middleware")
 const campgrounds = require("../controllers/campgrounds")
-const  multer   =  require ( 'multer' ) 
-const  upload  =  multer ( {  dest : 'uploads/'  } )
+const multer = require("multer")
+const { storage } = require("../cloudinary")
+const upload = multer({ dest: storage })
 
 router
   .route("/")
   .get(catchAsync(campgrounds.index)) // パスを指定しない
   // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
-.post(upload.array("image"),(req,res)=>{
-  console.log(req.body) //* url-encodedにしてたから　{}
-  console.log(req.files)
-  res.send("受け付けました")
-})
+  .post(upload.array("image"), (req, res) => {
+    console.log(req.body) //* url-encodedにしてたから　{}
+    console.log(req.files)
+    res.send("受け付けました")
+  })
 
 router.get("/new", isLoggedIn, campgrounds.renderNewForm)
 
@@ -23,8 +24,6 @@ router
   .get(catchAsync(campgrounds.showCampground))
   .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
   .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground))
-
-
 
 router.get("/:id/edit", isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm))
 
