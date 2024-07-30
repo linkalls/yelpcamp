8ad78f75cf7d663556360bd0c1b1c00ef29edc9c
poteto <naonyan1135@gmail.com>
2024-07-31 01:21:25 +0900

    あｄｄ

--- schemas.js
+++ schemas.js
@@ -7,11 +7,12 @@ module.exports.campgroundSchema = Joi.object({
     location: Joi.string().required(),
     description: Joi.string().required(),
   }).required(), //* という名前のあるキーを期待
+  deleteImages: Joi.array(),
 })
 
 module.exports.reviewSchema = Joi.object({
   review: Joi.object({
- rating: Joi.number().min(1).max(5).required(), //* review[rating]ってしてるから
- body: Joi.string().required()
-  }).required()
+    rating: Joi.number().min(1).max(5).required(), //* review[rating]ってしてるから
+    body: Joi.string().required(),
+  }).required(),
 })
