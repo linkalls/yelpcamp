622a08a1b8659c41bb5aa809388762ae7d8e0892
poteto <naonyan1135@gmail.com>
2024-07-31 03:17:35 +0900

    /fix bugs

--- public/js/showpagemap.js
+++ public/js/showpagemap.js
@@ -1,13 +1,13 @@
+mapboxgl.accessToken = mapToken
+const map = new mapboxgl.Map({
+  container: "map", // container ID
+  style: "mapbox://styles/mapbox/streets-v12", // style URL
+  center: campground.geometry.coordinates, // starting position [lng, lat]
+  zoom: 8, // starting zoom
+})
 
-  mapboxgl.accessToken = mapToken;
-  const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: campground.geometry.coordinates,// starting position [lng, lat]
-    zoom: 8, // starting zoom
-  });
-
-     // Create a default Marker and add it to the map.
-     const marker1 = new mapboxgl.Marker()
-     .setLngLat(campground.geometry.coordinates)
-     .addTo(map);
+// Create a default Marker and add it to the map.
+new mapboxgl.Marker()
+  .setLngLat(campground.geometry.coordinates)
+  .setPopup(new mapboxgl.Popup({ offset: 25 }).setHTML(`<h4>${campground.title}</h4>`))
+  .addTo(map)
