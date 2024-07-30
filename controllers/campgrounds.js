7da894f177f34f91530de99758bb3afbfd5bf958
poteto <naonyan1135@gmail.com>
2024-07-31 02:17:19 +0900

    ex

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,5 +1,8 @@
 const Campground = require("../models/campground")
-const {cloudinary} = require("../cloudinary")
+const { cloudinary } = require("../cloudinary")
+const mbxGeocodeing = require("@mapbox/mapbox-sdk/services/geocoding")
+const mapboxToken = process.env.MAPBOX_TOKEN
+const geocoder = mbxGeocodeing({ accessToken: mapboxToken })
 
 module.exports.index = async (req, res) => {
   const campgrounds = await Campground.find({})
@@ -31,13 +34,35 @@ module.exports.showCampground = async (req, res) => {
 
 module.exports.createCampground = async (req, res) => {
   const campground = new Campground(req.body.campground)
-  // console.log(req.user._id)
-  campground.images = req.files.map((f) => ({ url: f.path, filename: f.filename }))
-  campground.author = req.user._id
-  await campground.save()
-  // console.log(campground)
-  req.flash("success", "新しいキャンプ場を登録しました")
-  res.redirect(`/campgrounds/${campground._id}`)
+  const geoData = await geocoder
+    .forwardGeocode({
+      query: "愛知県名古屋市",
+      limit: 1,
+    })
+    .send()
+  console.log(geoData.body.features[0].geometry) //* { type: 'Point', coordinates: [ 136.902403, 35.187295 ] }
+  // features: [
+  //   {
+  //     id: 'place.2451572',
+  //     type: 'Feature',
+  //     place_type: [Array],
+  //     relevance: 0.99,
+  //     properties: [Object],
+  //     text: '名古屋市',
+  //     place_name: '名古屋市, Aichi, Japan',
+  //     matching_place_name: '名古屋市, 愛知県, Japan',
+  //     bbox: [Array],
+  //     center: [Array],
+  //     geometry: [Object],
+  //     context: [Array]
+  //   }
+  // ],
+  // campground.images = req.files.map((f) => ({ url: f.path, filename: f.filename }))
+  // campground.author = req.user._id
+  // await campground.save()
+  // // console.log(campground)
+  // req.flash("success", "新しいキャンプ場を登録しました")
+  // res.redirect(`/campgrounds/${campground._id}`)
 }
 
 module.exports.renderEditForm = async (req, res) => {
@@ -58,8 +83,8 @@ module.exports.updateCampground = async (req, res) => {
   // console.log(...imgs)
   await campground.save()
   if (req.body.deleteImages) {
-    for(let filename of req.body.deleteImages) {
-     await cloudinary.uploader.destroy(filename)
+    for (let filename of req.body.deleteImages) {
+      await cloudinary.uploader.destroy(filename)
     }
     await campground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } }) //* imagesのなかでfilenameがreq.body.deleteImagesの中と一致するものを取り除く
   }
